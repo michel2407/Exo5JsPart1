2c03793f7230b68ae94f4883806fed4c55bcd7bf
michel2407 <62596237+michel2407@users.noreply.github.com>
2020-05-04 13:22:55 +0200

    exo 5 corriger par Eddy

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+var frist, second, result;
+
+first = prompt('Entrer le premier chiffre :');
+second = prompt('Entrer le premier seconde :');
+résult = first % second;
+
+alert(result);
+// il est possible de le faire comme l'exercice 4 sous forme de 
